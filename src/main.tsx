218deeaea13abf8bbbe46969b48c0f250ef0f13e
FlawLessError <flawlesserrordev@gmail.com>
2025-03-13 22:36:27 +0000

    Fix some issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import RouterContextProvider from "./pages/RouterOutlet/RouterContextProvider";
+import RouterContextProvider from "./pages/Router/RouterContextProvider";
 
 import "./sass/main.scss";
 
