0efb17d2ed612e9b25cb4a5b311f359c5580b3ca
FlawLessError <flawlesserrordev@gmail.com>
2025-03-19 05:45:14 +0000

    Add refresh logic to NavBar website logo

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,23 +4,18 @@ import { NavHashLink } from "react-router-hash-link";
 import Logo from "../../assets/Logo.svg";
 import { ComponentPropsWithoutRef } from "react";
 import ColorModeSwitch from "../ColorModeSwitch/ColorModeSwitch";
-import { useAppDispatch } from "../../store/hooks";
-import { changeGenre } from "../../store/genre-slice";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   className?: string;
 } & ComponentPropsWithoutRef<"header">;
 
 const NavBar = (props: Props) => {
-  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   return (
     <header {...props} className={`${styles.header} ${props.className}`}>
-      <NavHashLink
-        to="/#"
-        onClick={dispatch(changeGenre(null))}
-        className={styles.logo}
-      >
+      <NavHashLink to="/#" onClick={() => navigate(0)} className={styles.logo}>
         <img src={Logo} alt="game hub logo" />
       </NavHashLink>
       <ColorModeSwitch />
