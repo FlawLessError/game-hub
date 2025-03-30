88521581608167e8db5328b33d188a78b5232e54
FlawLessError <flawlesserrordev@gmail.com>
2025-03-30 19:45:52 +0000

    Add onclick event for NavBar logo

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,19 +5,26 @@ import { NavHashLink } from "react-router-hash-link";
 import Logo from "../../assets/Logo.svg";
 import ColorModeSwitch from "../ColorModeSwitch/ColorModeSwitch";
 import GamesSearchInput from "../GamesSearchInput/GamesSearchInput";
+import { useAppDispatch } from "../../store/hooks";
+import { changeSearchQuery } from "../../store/gameQueries-slice";
 
 type Props = {
   className?: string;
 } & ComponentPropsWithoutRef<"header">;
 
 const NavBar = (props: Props) => {
+  const dispatch = useAppDispatch();
+  const navLinkHandler = () => {
+    dispatch(changeSearchQuery(""));
+  };
+
   return (
     <header
       {...props}
       className={`container ${styles.header} ${props.className}`}
       data-type="wide"
     >
-      <NavHashLink to="/#" className={styles.logo}>
+      <NavHashLink to="/#" onClick={navLinkHandler} className={styles.logo}>
         <img src={Logo} alt="game hub logo" />
       </NavHashLink>
       <GamesSearchInput />
